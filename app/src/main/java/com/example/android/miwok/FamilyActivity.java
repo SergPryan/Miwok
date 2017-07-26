ea635a838ee174ab6249592a73576b4326955580
Sergey Pryanichnikov <sergey.pryanichnikov@sibers.com[D>
2017-07-26 15:17:37 +0700

    first commit

--- app/src/main/java/com/example/android/miwok/FamilyActivity.java
+++ app/src/main/java/com/example/android/miwok/FamilyActivity.java
@@ -17,12 +17,30 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FamilyActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_family);
+        setContentView(R.layout.word_list);
+
+        List<Word> words = new ArrayList<Word>();
+        words.add(new Word(R.drawable.family_father,"father","әpә"));
+        words.add(new Word(R.drawable.family_mother,"mother","әṭa"));
+        words.add(new Word(R.drawable.family_son,"son","angsi"));
+        words.add(new Word(R.drawable.family_daughter,"daughter","tune"));
+        words.add(new Word(R.drawable.family_older_brother,"older brother","taachi"));
+        words.add(new Word(R.drawable.family_younger_brother,"younger brother","chalitti"));
+        words.add(new Word(R.drawable.family_older_sister,"older sister","ama"));
+        words.add(new Word(R.drawable.family_grandfather,"grandfather","paapa"));
+        words.add(new Word(R.drawable.family_grandmother,"family_grandmother","mamo"));
+        WordAdapter arrayAdapter = new WordAdapter(this,words,R.color.category_family);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(arrayAdapter);
     }
 }
--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -15,14 +15,99 @@
  */
 package com.example.android.miwok;
 
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.media.AudioManager.AUDIOFOCUS_LOSS_TRANSIENT;
+import static android.media.AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK;
 
 public class NumbersActivity extends AppCompatActivity {
+
+    private MediaPlayer mMediaPlayer;
+    private AudioManager mAudioManager;
+    private MediaPlayer.OnCompletionListener onCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mp) {
+            releaseMediaPlayer();
+        }
+    };
+
+    AudioManager.OnAudioFocusChangeListener afChangeListener =
+            new AudioManager.OnAudioFocusChangeListener() {
+                public void onAudioFocusChange(int focusChange) {
+                    if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
+                        releaseMediaPlayer();
+                    }
+                    else if (focusChange == AUDIOFOCUS_LOSS_TRANSIENT || focusChange == AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
+                        // Pause playback
+                        mMediaPlayer.stop();
+                        mMediaPlayer.seekTo(0);
+                    }  else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
+                        mMediaPlayer.start();
+                    }
+                }
+            };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_numbers);
+        setContentView(R.layout.word_list);
+        mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+
+        final List<Word> words = new ArrayList<Word>();
+        words.add(new Word(R.drawable.number_one,R.raw.number_one,"one","lutti"));
+        words.add(new Word(R.drawable.number_two,R.raw.number_two,"two","atiiko"));
+        words.add(new Word(R.drawable.number_three,R.raw.number_three,"three","tolookosu"));
+        words.add(new Word(R.drawable.number_four,R.raw.number_four,"four","oyyisa"));
+        words.add(new Word(R.drawable.number_five,R.raw.number_five,"five","massokka"));
+        words.add(new Word(R.drawable.number_six,R.raw.number_six,"six","temmokka"));
+        words.add(new Word(R.drawable.number_seven,R.raw.number_seven,"seven","kenekaku"));
+        words.add(new Word(R.drawable.number_eight,R.raw.number_eight,"eight","kawinta"));
+        words.add(new Word(R.drawable.number_nine,R.raw.number_nine,"nine","wo’e"));
+        words.add(new Word(R.drawable.number_ten,R.raw.number_ten,"ten","na’aacha"));
+        WordAdapter arrayAdapter = new WordAdapter(this,words,R.color.category_numbers);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Word word = words.get(position);
+                releaseMediaPlayer();
+                int result = mAudioManager.requestAudioFocus(afChangeListener,AudioManager.STREAM_MUSIC,AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+                if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED){
+//                    mAudioManager.registerMediaButtonEventReceiver(afChangeListener);
+                    mMediaPlayer = MediaPlayer.create(NumbersActivity.this,word.getmSoundResourceId());
+                    mMediaPlayer.start();
+                    Log.v("Numbers activity","Current world " +word);
+                    mMediaPlayer.setOnCompletionListener(onCompletionListener);
+                }
+            }
+        });
+
     }
+    private void releaseMediaPlayer(){
+        if(mMediaPlayer != null){
+            mMediaPlayer.release();
+            mMediaPlayer=null;
+            mAudioManager.abandonAudioFocus(afChangeListener);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        releaseMediaPlayer();
+    }
+
+
 }
